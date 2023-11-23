5696967f521f17a834f0735af55e12ceb42dbe7c
Salman Farshe <salman.zihad@involve.ai>
2023-11-24 00:30:26 +0600

    About Page

---
+++ about.php
@@ -0,0 +1,31 @@
+ <!-- about me start -->
+    <div id="About" class="about_me_wrapper">
+        <div class="about_me_info_wrapper">
+            <div class="">
+                <h1 class="about_me">Basic Info</h1>
+                <div class="border about_me_border"></div>
+            </div>
+            <div class="basic_info">
+                <p class="First_Name">First Name : <span>Salman</span></p>
+                <p class="last_name">Last Name : <span>Farshe</span></p>
+                <p class="Age">Age : <span class="age"></span></p>
+                <p class="Nationality">Nationality : <span>Bangladeshi</span></p>
+                <p class="language">Language : <span>Bangla, English, Hindi.</span></p>
+                <p class="Religion">Religion : <span>Only Islam</span></p>
+                <p class="address">Address : <span>Ashulia, Savar, Dhaka, Bangladesh.</span></p>
+                <p class="Phone">Phone : <span>+8801747289024, +8801827848492</span></p>
+                <p class="email">Email : <span>salmanzihad9024@gmail.com</span></p>
+            </div>
+            <div class="contact_me_in_about">
+                <span class="contact_button_info">For more information &rarr;</span>
+
+                <a class="contact_button contact_button_1" href="#Contact">Contact Me</a>
+            </div>
+            <div class="resume_wrap">
+                <p class="resume_wrap_contact_button_info">Get my sort hand <br> resume here</p>
+                <button class="contact_button contact_button_2"><a href="img/resume/Salman_Farshe-Resume.pdf"
+                        download="Salman_Farshe-Resume.pdf" class="resume_down">Download</a></button>
+            </div>
+        </div>
+    </div>
+    <!-- about me end -->
